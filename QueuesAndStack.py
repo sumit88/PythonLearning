6609fc03eecccd407c618d12293dbbc6bdde2f49
Sumit Chauhan <sumit.chauhan@datapassports.com>
2020-06-14 15:27:30 -0400

    added few more examples

---
+++ QueuesAndStack.py
@@ -0,0 +1,49 @@
+import sys
+
+
+class Solution:
+
+
+# Write your code here
+    a = []    #array
+    b = []    #queue
+
+    def pushCharacter(self, d):
+        self.a.insert(0, d)
+
+
+    def enqueueCharacter(self, d):
+        self.b += d
+
+    def popCharacter(self):
+        return self.a.pop()
+
+    def dequeueCharacter(self):
+        return self.b.pop()
+
+# read the string s
+s = input()
+# Create the Solution class object
+obj = Solution()
+
+l = len(s)
+# push/enqueue all the characters of string s to stack
+for i in range(l):
+    obj.pushCharacter(s[i])
+    obj.enqueueCharacter(s[i])
+
+isPalindrome = True
+'''
+pop the top character from stack
+dequeue the first character from queue
+compare both the characters
+'''
+for i in range(l // 2):
+    if obj.popCharacter() != obj.dequeueCharacter():
+        isPalindrome = False
+        break
+# finally print whether string s is palindrome or not.
+if isPalindrome:
+    print("The word, " + s + ", is a palindrome.")
+else:
+    print("The word, " + s + ", is not a palindrome.")
